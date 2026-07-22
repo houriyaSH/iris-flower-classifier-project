c84f4c229edc48103cdd4406bf43b8e3008b669e
houriyaSH <houriya.shiri@gmail.com>
2026-07-22 20:25:20 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let trainingData = [];
 let minValues = [];
 let maxValues = [];
-// خواندن دیتاست
 window.onload = function(){
     Papa.parse("./iris.csv",{
         download: true,
@@ -26,7 +25,6 @@ window.onload = function(){
         }
     });
 };
-// نرمال سازی داده‌ها
 function normalizeData(){
     for(let i = 0; i < 4; i++){
         let values = trainingData.map(item => item.features[i]);
@@ -40,7 +38,6 @@ function normalizeData(){
         });
     });
 }
-// محاسبه فاصله بین دو نمونه
 function distance(a,b){
     return Math.sqrt(
         (a[0]-b[0])**2 +
@@ -50,35 +47,29 @@ function distance(a,b){
     );
 }
 function predict(){
-    // گرفتن مقدارهای وارد شده توسط کاربر
     let values = [
         document.getElementById("sepalLength").value,
         document.getElementById("sepalWidth").value,
         document.getElementById("petalLength").value,
         document.getElementById("petalWidth").value
     ];
-    // بررسی خالی بودن ورودی‌ها
     for(let value of values){
         if(value.trim() === ""){
             alert("Please enter all flower measurements!");
             return;     
         }     
     }
-    // تبدیل رشته به عدد
     let input = values.map(Number);
-    // بررسی عدد بودن
     for(let value of input){
         if(isNaN(value)){
             alert("Please enter numbers only!");
             return;
         }
     }
-    // بررسی آماده بودن مدل
     if(trainingData.length === 0){
         alert("Model is not ready yet!");
         return;
     }
-    // بررسی محدوده منطقی مقادیر Iris
     if(
         input[0] < 4 || input[0] > 8 ||
         input[1] < 1.5 || input[1] > 5 ||
@@ -88,34 +79,26 @@ function predict(){
         alert("Values are outside the Iris flower range!");
         return;
     }
-    // نرمال کردن ورودی کاربر
     input = input.map((value,index)=>{
         return (value - minValues[index]) /
         (maxValues[index] - minValues[index]);
     });
-    // محاسبه فاصله با تمام نمونه‌ها
     let neighbors = trainingData.map(item => {
         return {
             label: item.label,
             dist: distance(input,item.features)
         };
     });
-    // مرتب کردن از نزدیک‌ترین
     neighbors.sort((a,b)=>a.dist-b.dist);
-    // تعداد همسایه‌ها
     let k = 5;
     let votes = {};
-    // رای گیری
     for(let i = 0; i < k; i++){
         let label = neighbors[i].label;
         votes[label] = (votes[label] || 0) + 1;
     }
-    // انتخاب بیشترین رای
     let prediction = Object.keys(votes).reduce((a,b)=>{
         return votes[a] > votes[b] ? a : b;
     });
-    // نمایش نتیجه
-
  let speciesName;
  let flowerEmoji;
  if(prediction === "Iris-setosa"){
